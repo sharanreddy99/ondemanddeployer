f70a539b3cf0f5a80efd7eade7e034dba9e4561d
Sharan Reddy <sharansaikonda99@gmail.com>
2023-07-23 00:11:55 -0400

    fix cors

--- main.go
+++ main.go
@@ -14,8 +14,8 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sns"
-	"github.com/beego/beego/plugins/cors"
 	beego "github.com/beego/beego/v2/server/web"
+	"github.com/beego/beego/v2/server/web/filter/cors"
 )
 
 func Subscribe() {
